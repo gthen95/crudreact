676dc50b10264f6752c734bf7c028777ff4766a7
Gerald Then <gthen@mdsoft.com.do>
2021-02-22 17:47:33 -0400

    Delete task

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{ useState } from 'react'
-import { isEmpty } from 'lodash'
+import { isEmpty,size } from 'lodash'
 import shortid from 'shortid'
 
 function App() {
@@ -22,6 +22,11 @@ function App() {
     setTask("")
   }
 
+  const deletetask= (id) =>{
+    const filteredTasks = tasks.filter(task => task.id !== id)
+    setTasks(filteredTasks)
+  }
+
   return (
     <div className="container mt-5">
       <h1>Tareas</h1>
@@ -30,17 +35,33 @@ function App() {
       <div className="row">
         <div className="col-8">
           <h4 className="text-center">Lista de Tareas</h4>
-          <ul className="list-group">
-            {
-              tasks.map((task)=>(
-                <li className="list-group-item" key={task.id}>
-                  <span className="lead">{task.name}</span>
-                  <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-                  <button className="btn btn-warning btn-sm float-right">Editar</button>
-                </li>
-              )) 
-            }
-          </ul>
+          {
+            size(tasks) == 0 ? (
+              <h5 className="text-center">Aun no hay tareas programadas.</h5>
+            ) : (
+              <ul className="list-group">
+                {
+                  tasks.map((task)=>(
+                    <li className="list-group-item" key={task.id}>
+                      <span className="lead">{task.name}</span>
+                      <button 
+                      className="btn btn-danger btn-sm float-right mx-2"
+                      onClick={()=> deletetask(task.id)}
+                      >
+                        Eliminar
+                      </button>
+                      <button 
+                      className="btn btn-warning btn-sm float-right"
+                      >
+                        Editar
+                      </button>
+                    </li>
+                  )) 
+                }
+              </ul>
+            )
+            
+          }
         </div>
         <div className="col-4">
         <h4 className="text-center">Formulario</h4>
